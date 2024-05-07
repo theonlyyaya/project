44e16aa38115ba93281ba6356bca3345ba53f400
Andr1mon <andreiamburwev@mail.ru>
2024-05-07 10:28:51 +0200

    New

--- client/revers-ia/src/app/services/api.service.ts
+++ client/revers-ia/src/app/services/api.service.ts
@@ -8,7 +8,7 @@ import { tap } from 'rxjs/operators';
 })
 export class ApiService {
   
-  private apiUrl = 'https://localhost:5000' //'https://fictional-rotary-phone-xg9x69vvr6jfgr-5000.app.github.dev/'////'https://project-2g6w.onrender.com'; // online
+  private apiUrl =  'https://project-1-gb5q.onrender.com' // 'https://localhost:5000' //'https://fictional-rotary-phone-xg9x69vvr6jfgr-5000.app.github.dev/'////'https://project-2g6w.onrender.com'; // online
 
   
 
