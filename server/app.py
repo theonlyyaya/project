d923e93d59afc723786b991ee0eab48701202a06
Andr1mon <andreiamburwev@mail.ru>
2024-05-07 10:51:49 +0200

    new

--- server/app.py
+++ server/app.py
@@ -212,7 +212,7 @@ class ReversiGrid():
         elif (player == 'Medium'):
             conf['player']= ''
         elif (player == 'Hard'):
-            conf['player']= '.models\\Hard.pt'
+            conf['player']= 'server\\models\\Hard.pt'
         
         model = torch.load(conf['player'],map_location=torch.device('cpu'))
         model.eval()
@@ -253,7 +253,7 @@ class ReversiGrid():
         elif (player == 'Medium'):
             conf['player']= ''
         elif (player == 'Hard'):
-            conf['player']= '.models\\Hard.pt'
+            conf['player']= 'server\\models\\Hard.pt'
         
         model = torch.load(conf['player'],map_location=torch.device('cpu'))
         model.eval()
